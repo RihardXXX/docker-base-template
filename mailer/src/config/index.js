bcf9a9b99d20266c1af43a72289b5f1fa700b70e
RihardXXX <yusupa.akaeva@yandex.ru>
2023-05-10 20:36:44 +0300

    feat: over base template for projects on docker master

--- mailer/src/config/index.js
+++ mailer/src/config/index.js
@@ -1,6 +1,5 @@
 // получаем из docker compose.yaml снаружи
 module.exports.port = process.env.PORT;
-module.exports.host = process.env.HOST;
 module.exports.dbPath = process.env.MONGO_URL;
-module.exports.apiUrl = process.env.API_URL;
-module.exports.apiMailer = process.env.API_MAILER;
+module.exports.login = process.env.LOGIN;
+module.exports.password = process.env.PASSWORD;
